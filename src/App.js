db5b4b704e9d93500dd7c9552683c45849858485
pierre <pierre.couderc06@gmail.com>
2025-04-11 18:35:18 +0200

    amél menu burger + contenu footer (mail ouvrant + Link monte haut de page + liens réalisations->portfolio) + début travail sur page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,46 +1,65 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Achievements from "./pages/Achievements";
 import Contact from "./pages/Contact";
 import LegalNotices from "./pages/LegalNotices";
-import { slide as Menu } from 'react-burger-menu';
+import { fallDown as Menu } from 'react-burger-menu';
 import './App.css';
 
-class Example extends React.Component {
-  showSettings(event) {
-    event.preventDefault();
 
+let styles = {
+  bmMenuWrap: {
+    height: 'max-content',
+    transition: '0.2s ease-in-out',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0)'
+  },
+  bmPageWrap: {
+    background: 'rgba(0, 0, 0, 0)'
   }
 }
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+
+}
 export default function App() {
   return (
-    <div className="App" id="outer-container">
+    <div className="App">
 
       <header className="App-header">
-        <div id='with-burger-menu'><Menu right customBurgerIcon={<img src="img/icon.svg" />}>
-          <Link to="/">Accueil</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/achievements">Réalisations</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/legal-notices">Mentions légales</Link>
-        </Menu>
-        </div>
-        <div id='no-burger-menu'>
-          <Link to="/">Accueil</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/achievements">Réalisations</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/legal-notices">Mentions légales</Link>
+        <nav id='navbar' className="navbar navbar-dark bg-dark">
+          <span className="navbar-brand mb-0 h1" id='logo'>JOHN DOE</span>
+          <div id='with-burger-menu'>
+            <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} right width={'102%'} styles={styles}>
+              <NavLink to="/" activeClassName="active">Accueil</NavLink>
+              <NavLink to="/services" activeClassName="active">Services</NavLink>
+              <NavLink to="/achievements" activeClassName="active">Réalisations</NavLink>
+              <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+              <NavLink to="/legal-notices" activeClassName="active">Mentions légales</NavLink>
 
-        </div>
+            </Menu>
+          </div>
+          <div id='no-burger-menu'>
+            <NavLink to="/" activeClassName="active" >Accueil</NavLink>
+            <NavLink to="/services" activeClassName="active">Services</NavLink>
+            <NavLink to="/achievements" activeClassName="active">Réalisations</NavLink>
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+            <NavLink to="/legal-notices" activeClassName="active">Mentions légales</NavLink>
 
+          </div>
+        </nav>
       </header>
-      <main className="App-body">
-        <p>Body</p>
+      <main className="Appbody" id="page-wrap">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/services" element={<Services />}></Route>
@@ -48,9 +67,33 @@ export default function App() {
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/legal-notices" element={<LegalNotices />}></Route>
         </Routes>
-      </main>
+      </main >
       <footer className="App-footer">
-        <p>Footer</p>
+        <div id='footer-1'>
+          <p><strong>John Doe</strong></p>
+          <p>40 rue Laure Diebold</p>
+          <p>69009 Lyon, France</p>
+          <p>10 20 30 40 50</p>
+          <a href="mailto:john.doe@gmail.com" id='mail-to'>john.doe@gmail.com</a>
+        </div>
+        <div id='footer-2'>
+          <p><strong>Liens utiles</strong></p>
+          <ScrollToTop />
+          <NavLink to="/" activeClassName="active">Accueil</NavLink>
+          <NavLink to="/services" activeClassName="active">Services</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisations</NavLink>
+          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+          <NavLink to="/legal-notices" activeClassName="active">Mentions légales</NavLink>
+        </div>
+        <div id='footer-3'>
+          <p><strong>Mes dernières réalisations</strong></p>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 1</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 2</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 3</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 4</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 5</NavLink>
+          <NavLink to="/achievements" activeClassName="active">Réalisation 6</NavLink>
+        </div>
       </footer>
     </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 const Home = () => {
     return (
-        <div>
-            <h1>Test Home - H1</h1>
+        <div id="home_main">
+            <div id="home_part-1">
+                <div id="home-background_part-1">
+                    <h1>Bonjour, je suis John Doe</h1>
+                    <h2>Développeur web full stack</h2>
+                </div></div>
+            <div id="home_part-2"><h1>Test Home - partie 2</h1></div>
         </div>
     )
 }
